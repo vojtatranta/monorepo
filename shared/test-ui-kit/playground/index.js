4b40a7d2037e8a82c99c0695266d2f33172551d9
Vojtěch Tranta <vojta.tranta@gmail.com>
2019-12-12 13:00:17 +0100

    add ui kit playground

---
+++ shared/test-ui-kit/playground/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/index.css';
+import App from '../src/App';
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
